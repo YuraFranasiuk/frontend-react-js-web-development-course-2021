c7657108dea8022d57dd46ecc504394c3063ab90
Yura Franasiuk <yura.franasiuk.com>
2022-09-01 23:56:43 +0300

    compleated task3

---
+++ Practice: Fulfil missing operators in equations and expressions/task3.js
@@ -0,0 +1,32 @@
+function printSeasonByMonth(month) {
+    switch (month) {
+        case 'DECEMBER':
+        case 'JANUARY':
+        case 'FEBRUARY':
+            console.log('winter')
+            break;
+        case 'MARCH':
+        case 'APRIL':
+        case 'MAY':
+            console.log('spring')
+            break;
+        case 'JUN':
+        case 'JULY':
+        case 'AUGUST':
+            console.log('summer')
+            break;
+        case 'SEPTEMBER':
+        case 'OCOTBER':
+        case 'NOVEMBER':
+            console.log('autumn')
+            break;
+        default:
+            console.log('is not a month')
+            break;
+    }
+}
+
+printSeasonByMonth("SEPTEMBER");
+printSeasonByMonth("NOVEMBER");
+printSeasonByMonth("JULY");
+printSeasonByMonth("APRIL");
