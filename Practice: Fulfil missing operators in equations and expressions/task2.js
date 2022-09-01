fa74cdccc16a9ff7ab62907c09568efe2ffaa4b3
Yura Franasiuk <yura.franasiuk.com>
2022-09-01 23:55:16 +0300

    compleated task2

---
+++ Practice: Fulfil missing operators in equations and expressions/task2.js
@@ -0,0 +1,11 @@
+function calculateSumOfArray() {
+    const initialArray = [3, 2, "2", null, 1.5, 9.5, undefined];
+
+    const sum = initialArray.reduce((previousValue, currentValue) => {
+        return typeof currentValue == 'number' ? previousValue + currentValue : previousValue
+    }, 0);
+
+    console.log(sum)
+}
+
+calculateSumOfArray();
