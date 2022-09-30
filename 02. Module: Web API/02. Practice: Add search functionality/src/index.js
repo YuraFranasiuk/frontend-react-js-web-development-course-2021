3676c16f561dd5d46e2ed07b92a5f82ef3fca03f
Yura Franasiuk <y.franasiuk@gmail.com>
2022-09-30 23:22:17 +0300

    compleate

---
+++ 02. Module: Web API/02. Practice: Add search functionality/src/index.js
@@ -0,0 +1,52 @@
+import api from './api.js';
+import * as webApi from './webApi.js';
+
+const movieSearchForm = document.querySelector('#movieSearchForm');
+const movieCatalogLoadMoreButton = document.querySelector('#movieCatalogLoadMoreButton');
+
+let movie = null;
+let page = 1;
+
+movieSearchForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const data = new FormData(evt.target);
+    if (!data.get('movie')) {
+        return;
+    }
+
+    movie = data.get('movie');
+    page = 1;
+
+    webApi.clearMovieCatalog();
+    renderMovieCatalogPage(movie, page);
+});
+
+movieCatalogLoadMoreButton.addEventListener('click', (evt) => {
+    page += 1;
+
+    renderMovieCatalogPage(movie, page);
+});
+
+async function renderMovieCatalogPage(movie, page) {
+    const pageData = await api.fetchMoviesBySearchText(movie, page);
+    if (pageData.total_results) {
+        webApi.showResultsMessage(`Results (${pageData.total_results})`)
+    } else {
+        webApi.showResultsMessage(`No results for (${movie})`);
+
+        return;
+    }
+
+    pageData.results.forEach((movie) => {
+        webApi.insertMovieCard(
+            movie.original_title, api.create_poster_url(movie.poster_path)
+        );
+
+        if (pageData.total_pages === pageData.page) {
+            webApi.hideLoadMoreButton();
+        } else {
+            webApi.showLoadMoreButton();
+        }
+    });
+}
